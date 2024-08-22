2846fa865eed3fe179cfaa50612e92ed7cb5481f
Khalid Mahmood <khalidm449@gmail.com>
2024-08-22 10:59:30 -0500

    new .py "iterables

---
+++ iterables.py
@@ -0,0 +1,8 @@
+#an object/ collection that can return its elements one at a time, allowing
+#it to be iterated over in a loop
+
+
+numbers = [1, 2, 3 ,4 ,5]
+
+for number in numbers: #try to keep naming convention similar to what you're iterating 
+    print(number)
